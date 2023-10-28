d118e5bd2b40c835bc7e86201a7b9ba84767ba87
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2023-10-28 12:42:07 -0400

    finished up all button functions, fix output display formating and deleting privious operand after computation

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ class Calculator{
     }
     
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 //add nunber when clicked
     appendNumber(number){
@@ -64,9 +64,34 @@ class Calculator{
         this.previousOperand = ''
     }
 
+    getDisplayNumber(number){
+        const StringNumber = number.toString()
+        const integerDigits = parseFloat(StringNumber.split('.')[0])
+        const decimalDigits = StringNumber.split('.')[1]
+        let integerDisplay
+        //nothing is entered
+        if(isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0})
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = 
+            this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null){
+            this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
 
@@ -90,6 +115,16 @@ operationButtons.forEach(button => {
 //when the equal button is clicked, the operation button is added
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
 
