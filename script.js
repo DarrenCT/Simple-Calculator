04967efd3add907421dd973d4dcef8c300d82ed6
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2023-10-28 12:05:25 -0400

    Added Calculator class and function calls for number and operation buttons

--- script.js
+++ script.js
@@ -4,4 +4,69 @@ const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandButton = document.querySelector('[data-current-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    
+    delete(){
+
+    }
+//add nunber when clicked
+    appendNumber(number){
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+//add operator when clicked
+    chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+//execute operation
+    compute(){
+
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
+    }
+}
+
+//create calculator object
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+//when buttons are clicked, the text on the button is added
+numberButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+//when buttons are clicked, the operation on the button is added
+operationButtons.forEach(button => {
+    button.addEventListener('click',() => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute
+    calculator.updateDisplay
+})
+
